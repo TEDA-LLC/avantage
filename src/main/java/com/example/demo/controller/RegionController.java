0d7cd8b126cd9549b409e2e0caa3405f74e714fd
AbdusamadMansurov <abdusamat.mansurov@gmail.com>
2023-08-22 13:01:52 +0500

    first commit

---
+++ src/main/java/com/example/demo/controller/RegionController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ApiResponse;
+import com.example.demo.repository.RegionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/region")
+@RequiredArgsConstructor
+public class RegionController {
+
+    private final RegionRepository regionRepository;
+
+    @GetMapping
+    public ResponseEntity<?> getAll(){
+        return ResponseEntity.ok(ApiResponse.builder().
+                message("Here").
+                status(200).
+                success(true).
+                data(regionRepository.findAll()).
+                build());
+    }
+
+}
